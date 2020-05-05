1d649c4dcfbf223e350c5dfb2c0861263280bffe
luaifei <lu.aifei@thoughtworks.com>
2020-05-05 13:58:25 +0800

    Fix issue: fix no directory or file

--- ci/static-code-check.sh
+++ ci/static-code-check.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p test-results
-flake8 --tee --output-file test-results/static-code-result.txt ./src
+flake8 --tee --output-file test-results/static-code-result.txt *.py
